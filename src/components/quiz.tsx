57c33bb5b53d2561991a8f8115447245215a2c76
f-asadov <farxad-214-a@mail.ru>
2022-12-13 23:59:06 +0400

    add progress bar

--- src/components/quiz.tsx
+++ src/components/quiz.tsx
@@ -1,6 +1,7 @@
 import { ICharacter, IQuestion } from '../interfaces'
 import '../styles/quiz-style.css'
 import questions from '../constansts/question.json'
+import { useState } from 'react';
 
 type QuizPropsType = {
     person: ICharacter;
@@ -8,6 +9,14 @@ type QuizPropsType = {
 
 const Quiz = (props: QuizPropsType) => {
     const quiestionArray = questions.questions
+    const [a,A] = useState<number>(0)
+    const [step,seStep] = useState<number>(0)
+    
+    const currentQuestion = quiestionArray[step]
+    
+    const onClickAnswer = (index:number) =>{
+        
+    }
 
     return <div className="question-container">
         <div className="top-person">
@@ -19,13 +28,11 @@ const Quiz = (props: QuizPropsType) => {
 
         </div>
         <div className='question-list'>
-            {quiestionArray.map((question: IQuestion) => {
-                return <div className='question'> {question.title}
-                    {question.answers.map((answer) => {
-                        return<span className='answer'>{answer.answer}</span>
-                    })}
-                </div>
-            })}
+           <progress value={a} max={100} >ss</progress>
+           <h1>{currentQuestion.title}</h1>
+           <ul  onClick={()=>A(a+20)}>
+            {currentQuestion.answers.map((answer,index)=><li key={answer.answer} onClick={()=>onClickAnswer(index)}>{answer.answer}</li>)}
+           </ul>
         </div>
 
     </div>
